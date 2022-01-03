832bb33c27f86e37e08622e112c6d0a2f6d52754
lewismd13 <72758390+lewismd13@users.noreply.github.com>
2022-01-03 12:27:44 -0600

    add time-spinner delevel

--- src/lib.ts
+++ src/lib.ts
@@ -366,6 +366,7 @@ export function ensureOde(turns: number) {
 export function kill() {
   return Macro.trySkill($skill`Curse of Weaksauce`)
     .trySkill($skill`Micrometeorite`)
+    .tryItem($item`Time-Spinner`)
     .trySkill($skill`Sing Along`)
     .trySkill($skill`Stuffed Mortar Shell`)
     .trySkill($skill`Saucestorm`)

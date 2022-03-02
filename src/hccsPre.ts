d77fd0245c1844b856846e52a11051dbc885521a
lewismd13 <72758390+lewismd13@users.noreply.github.com>
2022-03-02 12:28:53 -0600

    safari before jumping

--- src/hccsPre.ts
+++ src/hccsPre.ts
@@ -7,8 +7,9 @@ import {
     myInebriety,
     pvpAttacksLeft,
     use,
+    useSkill,
 } from "kolmafia";
-import { $item, Clan } from "libram";
+import { $item, $skill, Clan, get } from "libram";
 
 Clan.join("Alliance from Hell");
 
@@ -19,6 +20,39 @@ if (pvpAttacksLeft() > 0) {
 
 if (myInebriety() <= inebrietyLimit() || myFullness() < fullnessLimit()) {
     throw "are you sure you want to ascend? you have some open organ space";
+}
+
+const playerIDs: string[] = [
+    "phreddrickkv2",
+    "2548033",
+    "Phillammon",
+    "2705901",
+    "ReverKiller",
+    "beldur",
+    "887028",
+    "786069",
+    "1197090",
+    "437479",
+    "playultm8",
+    "busta_rhymes",
+    "644996",
+    "kenny kamAKAzi",
+    "SSBBHax",
+    "1937905",
+    "2766368",
+    "2203016",
+    "1972588",
+    "Butts McGruff",
+    "burningbman",
+    "2533291",
+    "1741165",
+    "1993636",
+    "2339258",
+    "DanceCommander6",
+];
+
+while ($skill`Experience Safari`.timescast < get("skillLevel180")) {
+    useSkill($skill`Experience Safari`, 1, playerIDs[Math.round(Math.random() * playerIDs.length)]);
 }
 
 if (myGardenType() !== "peppermint") {

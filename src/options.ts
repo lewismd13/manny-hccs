dcafe84aba034540aadacef57dfdd3cf29b678e9
lewismd13 <72758390+lewismd13@users.noreply.github.com>
2022-03-25 08:14:05 -0500

    halloween option

--- src/options.ts
+++ src/options.ts
@@ -3,6 +3,7 @@ export const globalOptions = {
     debug: false,
     hardcorePvp: true,
     workshed: `Asdon`,
+    halloween: false,
 };
 // debug is bare bones now, would like to add more
 // next level would be parsing but ugh

5705ba909ae8db5a35196e529b809549313c61f1
lewismd13 <72758390+lewismd13@users.noreply.github.com>
2022-04-28 11:23:45 -0500

    combine hccsPre and hccsAscend

--- src/hccsAscend.ts
+++ src/hccsAscend.ts
@@ -1,5 +1,29 @@
-import { equippedItem, print, wait } from "kolmafia";
-import { $class, $item, $slot, ascend, Lifestyle, Paths, prepareAscension } from "libram";
+import {
+    cliExecute,
+    equippedItem,
+    fullnessLimit,
+    inebrietyLimit,
+    myFullness,
+    myGardenType,
+    myInebriety,
+    print,
+    pvpAttacksLeft,
+    use,
+    useSkill,
+    wait,
+} from "kolmafia";
+import {
+    $class,
+    $item,
+    $skill,
+    $slot,
+    ascend,
+    Clan,
+    get,
+    Lifestyle,
+    Paths,
+    prepareAscension,
+} from "libram";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function main(args = ""): void {
@@ -11,6 +35,59 @@ export function main(args = ""): void {
 
     if (args.split(" ").includes("sauceror")) {
         myClass = $class`Sauceror`;
+    }
+
+    Clan.join("Alliance from Hell");
+
+    if (pvpAttacksLeft() > 0) {
+        cliExecute("uberpvpoptimizer");
+        cliExecute("swagger");
+    }
+
+    if (myInebriety() <= inebrietyLimit() || myFullness() < fullnessLimit()) {
+        throw "are you sure you want to ascend? you have some open organ space";
+    }
+
+    const playerIDs: string[] = [
+        "phreddrickkv2",
+        "2548033",
+        "Phillammon",
+        "2705901",
+        "ReverKiller",
+        "beldur",
+        "887028",
+        "786069",
+        "1197090",
+        "437479",
+        "playultm8",
+        "busta_rhymes",
+        "644996",
+        "kenny kamAKAzi",
+        "SSBBHax",
+        "1937905",
+        "2766368",
+        "2203016",
+        "1972588",
+        "Butts McGruff",
+        "burningbman",
+        "2533291",
+        "1741165",
+        "1993636",
+        "2339258",
+        "DanceCommander6",
+    ];
+
+    while ($skill`Experience Safari`.timescast < get("skillLevel180")) {
+        useSkill(
+            $skill`Experience Safari`,
+            1,
+            playerIDs[Math.round(Math.random() * playerIDs.length)]
+        );
+    }
+
+    if (myGardenType() !== "peppermint") {
+        cliExecute("garden pick");
+        use($item`Peppermint Pip Packet`);
     }
 
     if (equippedItem($slot`bootskin`) !== $item`frontwinder skin`) {
